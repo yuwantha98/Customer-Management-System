3b75a995a57c79f131771b01d503e2e4d8a24ca7
yuwantha98 <yuwantharavihara123@gmail.com>
2025-02-25 21:33:23 +0530

    first commit

---
+++ src/main/java/edu/icet/crm/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package edu.icet.crm.controller;
+
+import edu.icet.crm.dto.Customer;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    public void addCustomer(Customer customer){
+
+    }
+}
