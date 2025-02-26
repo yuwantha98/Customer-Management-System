00645da433aac0abb44502452c5be608af297a9e
yuwantha98 <yuwantharavihara123@gmail.com>
2025-02-26 13:23:17 +0530

    create controller class

--- src/main/java/edu/icet/crm/controller/CustomerController.java
+++ src/main/java/edu/icet/crm/controller/CustomerController.java
@@ -1,12 +1,26 @@
 package edu.icet.crm.controller;
 
 import edu.icet.crm.dto.Customer;
-import org.springframework.web.bind.annotation.RestController;
+import edu.icet.crm.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/customer")
 public class CustomerController {
 
-    public void addCustomer(Customer customer){
+    final CustomerService service;
 
+    @PostMapping("/add")
+    public void addCustomer(@RequestBody Customer customer){
+        service.addCustomer(customer);
+    }
+
+    @GetMapping("/get-all")
+    public List<Customer> getAll(){
+        return service.getAll();
     }
 }
