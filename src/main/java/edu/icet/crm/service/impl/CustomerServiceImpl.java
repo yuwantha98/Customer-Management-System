0169eaa3eaca87225bd81b3179f9c5fe0f506c8e
yuwantha98 <yuwantharavihara123@gmail.com>
2025-02-28 21:26:18 +0530

    update

--- src/main/java/edu/icet/crm/service/impl/CustomerServiceImpl.java
+++ src/main/java/edu/icet/crm/service/impl/CustomerServiceImpl.java
@@ -34,4 +34,44 @@ public class CustomerServiceImpl implements CustomerService {
 
         return customerList;
     }
+
+    @Override
+    public void deleteCustomer(Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateCustomer(Customer customer) {
+        customerRepository.save(mapper.map(customer,CustomerEntity.class));
+    }
+
+    @Override
+    public Customer searcgById(Integer id) {
+        return mapper.map(customerRepository.findById(id), Customer.class);
+    }
+
+    @Override
+    public List<Customer> searchByName(String name) {
+        List<CustomerEntity> byName = customerRepository.findByName(name);
+        List<Customer> customerList = new ArrayList<>();
+
+        byName.forEach(customerEntity -> {
+            customerList.add(mapper.map(customerEntity, Customer.class));
+        });
+
+        return customerList;
+    }
+
+    @Override
+    public List<Customer> searchByAddress(String address) {
+        List<CustomerEntity> byAddress = customerRepository.findByAddress(address);
+        List<Customer> customerList = new ArrayList<>();
+
+        byAddress.forEach(customerEntity -> {
+            customerList.add(mapper.map(customerEntity, Customer.class));
+        });
+
+        return customerList;
+    }
+
 }
