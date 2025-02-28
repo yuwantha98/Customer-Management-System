ce417d0a68a1ac99c5264ba63e470061515df267
yuwantha98 <yuwantharavihara123@gmail.com>
2025-02-28 21:25:38 +0530

    update

--- src/main/java/edu/icet/crm/controller/CustomerController.java
+++ src/main/java/edu/icet/crm/controller/CustomerController.java
@@ -3,18 +3,21 @@ package edu.icet.crm.controller;
 import edu.icet.crm.dto.Customer;
 import edu.icet.crm.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 @RequestMapping("/customer")
 public class CustomerController {
 
     final CustomerService service;
 
     @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
     public void addCustomer(@RequestBody Customer customer){
         service.addCustomer(customer);
     }
@@ -22,5 +25,32 @@ public class CustomerController {
     @GetMapping("/get-all")
     public List<Customer> getAll(){
         return service.getAll();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deleteCustomer(@PathVariable Integer id){
+        service.deleteCustomer(id);
+    }
+
+    @PutMapping("/Update-customer")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void updateCustomer(@RequestBody Customer customer){
+        service.updateCustomer(customer);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    public Customer searchById(@PathVariable Integer id){
+        return service.searcgById(id);
+    }
+
+    @GetMapping("/search-by-name/{name}")
+    public List<Customer> searchByName(@PathVariable String name){
+        return service.searchByName(name);
+    }
+
+    @GetMapping("/search-by-address/{address}")
+    public List<Customer> searchByAddress(@PathVariable String address){
+        return service.searchByAddress(address);
     }
 }
